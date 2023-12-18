9ca8917910b396f502799900e8523ec31d50925a
LevDiakov <381208dl@gmail.com>
2023-12-18 15:42:27 -0300

    добавил модель для фильтра

--- src/main.js
+++ src/main.js
@@ -4,12 +4,13 @@ import { mockOffers } from './mock/offers-mock.js';
 import DestinationsModel from './model/destinations-model.js';
 import OffersModel from './model/offers-model.js';
 import PointsModel from './model/points-model.js';
+import FilterModel from './model/filter-model.js';
 import FilterPresenter from './presenter/filter-presenter.js';
 import { mockPoints } from './mock/points-mock.js';
 const tripEventsContainer = document.querySelector('.trip-events');
 const filterContainer = document.querySelector('.trip-controls__filters');
 
-
+const filterModel = new FilterModel();
 const destinationsModel = new DestinationsModel(mockDestinations);
 const offersModel = new OffersModel(mockOffers);
 const pointsModel = new PointsModel(mockPoints);
