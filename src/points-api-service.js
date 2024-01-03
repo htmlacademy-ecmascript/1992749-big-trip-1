5fd9a1abd3d7c2e1a55c4812c71c74f1a3a18560
LevDiakov <381208dl@gmail.com>
2024-01-03 19:38:59 -0300

    создал класс для работы с API

---
+++ src/points-api-service.js
@@ -0,0 +1,26 @@
+import ApiService from './framework/api-service';
+
+const Method = {
+  GET: 'GET',
+  PUT: 'PUT',
+};
+
+export default class PointsApiService extends ApiService {
+  get points() {
+    return this._load({url: 'big-trip/points'})
+      .then(ApiService.parseResponse);
+  }
+
+  async updateTask(point) {
+    const response = await this._load({
+      url: `big-trip/points/${point.id}`,
+      method: Method.PUT,
+      body: JSON.stringify(point),
+      headers: new Headers({'Content-Type': 'application/json'}),
+    });
+
+    const parsedResponse = await ApiService.parseResponse(response);
+
+    return parsedResponse;
+  }
+}
