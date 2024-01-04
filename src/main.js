891c0facba247c04f64bb33fadd6148a9a684840
LevDiakov <381208dl@gmail.com>
2024-01-04 12:11:03 -0300

    сделал заготовку адаптера для клиента

--- src/main.js
+++ src/main.js
@@ -15,11 +15,6 @@ const END_POINT = 'https://20.ecmascript.pages.academy/big-trip';
 const tripEventsContainer = document.querySelector('.trip-events');
 const filterContainer = document.querySelector('.trip-controls__filters');
 
-const pointsApiService = new PointsApiService(END_POINT, AUTHORIZATION);
-//console.log(pointsApiService.points)
-pointsApiService.points.then((points) => {
-  console.log(points[0]);
-});
 const filterModel = new FilterModel();
 const destinationsModel = new DestinationsModel(mockDestinations);
 const offersModel = new OffersModel(mockOffers);
@@ -45,4 +40,39 @@ listPresenter.init();
 
 getNewEvent(listPresenter);
 
+/*
+const pointsApiService = new PointsApiService(END_POINT, AUTHORIZATION);
+const test = {
+  'id': '4f67e4f6-9737-4e7d-9916-e7ec528295c7',
+  'type': 'flight',
+  'date_from': '2024-01-02T10:22:57.061Z',
+  'date_to': '2024-01-03T08:53:19.828Z',
+  'destination': '910ecfc8-0810-4519-8f74-6cdd2bbee1a1',
+  'base_price': 300,
+  'is_favorite': true,
+  'offers': [
+    '18f241d3-847f-4bfd-b977-a6d97e63d7fb',
+    '57f24d44-e1cd-46a5-a442-cd7dea92aa23'
+  ]
+};
+function adaptToClient(point) {
+  const adaptedPoint = {...point,
+    dateFrom: point['date_from'],
+    dateTo: point['date_to'],
+    basePrice: point['base_price'],
+    isFavorite: point['is_favorite'],
+  };
 
+  // Ненужные ключи мы удаляем
+  delete adaptedPoint['date_from'];
+  delete adaptedPoint['date_to'];
+  delete adaptedPoint['base_price'];
+  delete adaptedPoint['is_favorite'];
+
+  return adaptedPoint;
+}
+//console.log(adaptToClient(test))
+pointsApiService.points.then((points) => {
+  console.log(points.map((point) => adaptToClient(point)));
+});
+*/
